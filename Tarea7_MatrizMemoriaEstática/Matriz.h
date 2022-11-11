770221e6a130d00582304fdd45cfd9e889d2538e
mspadron <105258775+mspadron@users.noreply.github.com>
2022-11-10 23:15:38 -0500

    Add files via upload

---
+++ Tarea7_MatrizMemoriaEstática/Matriz.h
@@ -0,0 +1,35 @@
+/***********************************************************************
+UFA - ESPE
+AUTORES: Matías Padrón,Sebastian Torres
+FECHA DE CREACIÓN: 10/11/2022
+FECHA DE MODIFICACIÓN: 10/11/2022
+Tarea7: Operaciones Matriz con memoria estática
+GITHUB: Padron-Torres-Primer-Parcial-Estructuras
+ ***********************************************************************/
+
+#if !defined(__Class_Diagram_1_Matriz_h)
+#define __Class_Diagram_1_Matriz_h
+#include "ValidacionDatos.cpp"
+
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include <ctime>
+using namespace std;
+class Matriz 
+{
+public:
+	Matriz();
+   ~Matriz();
+    void suma();
+	void multiplicacion();
+protected:
+private:
+   
+
+
+};
+
+#endif
