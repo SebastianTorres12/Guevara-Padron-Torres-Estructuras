18ac1791a1a8257e19b87abcb83683ab0a925fe8
mspadron <105258775+mspadron@users.noreply.github.com>
2022-11-19 16:37:13 -0500

    Add files via upload

---
+++ ProyectoP1/NominaSimple.cpp
@@ -0,0 +1,211 @@
+/***********************************************************************
+UFA - ESPE
+AUTORES: Matías Padrón,Sebastian Torres
+FECHA DE CREACIÓN: 19/11/2022
+FECHA DE MODIFICACIÓN: 19/11/2022
+Proyecto Unidad 1-Rol de pagos
+GITHUB: Padron-Torres-Primer-Parcial-Estructuras
+ ***********************************************************************/
+
+#include "NominaSimple.h"
+
+////////////////////////////////////////////////////////////////////////
+// Name:       NominaSimple::NominaSimple()
+// Purpose:    Implementation of NominaSimple::NominaSimple()
+// Return:     
+////////////////////////////////////////////////////////////////////////
+
+NominaSimple::NominaSimple()
+{
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       NominaSimple::~NominaSimple()
+// Purpose:    Implementation of NominaSimple::~NominaSimple()
+// Return:     
+////////////////////////////////////////////////////////////////////////
+
+NominaSimple::~NominaSimple()
+{
+   // TODO : implement
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       NominaSimple::insertarPorCabeza()
+// Purpose:    Implementation of NominaSimple::insertarPorCabeza()
+// Return:     int
+////////////////////////////////////////////////////////////////////////
+
+int NominaSimple::insertarPorCabeza(void)
+{
+   // TODO : implement
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       NominaSimple::eliminarPorCabeza()
+// Purpose:    Implementation of NominaSimple::eliminarPorCabeza()
+// Return:     int
+////////////////////////////////////////////////////////////////////////
+
+int NominaSimple::eliminarPorCabeza(void)
+{
+   // TODO : implement
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       NominaSimple::eliminarPorCola()
+// Purpose:    Implementation of NominaSimple::eliminarPorCola()
+// Return:     int
+////////////////////////////////////////////////////////////////////////
+
+int NominaSimple::eliminarPorCola(void)
+{
+   // TODO : implement
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       NominaSimple::mostrarNominaS()
+// Purpose:    Implementation of NominaSimple::mostrarNominaS()
+// Return:     int
+////////////////////////////////////////////////////////////////////////
+
+int NominaSimple::mostrarNominaS(void)
+{
+   // TODO : implement
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       NominaSimple::calcularIess()
+// Purpose:    Implementation of NominaSimple::calcularIess()
+// Return:     double
+////////////////////////////////////////////////////////////////////////
+
+double NominaSimple::calcularIess(void)
+{
+   // TODO : implement
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       NominaSimple::calcularHorasSup()
+// Purpose:    Implementation of NominaSimple::calcularHorasSup()
+// Return:     double
+////////////////////////////////////////////////////////////////////////
+
+double NominaSimple::calcularHorasSup(void)
+{
+   // TODO : implement
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       NominaSimple::calcularHorasExtra()
+// Purpose:    Implementation of NominaSimple::calcularHorasExtra()
+// Return:     double
+////////////////////////////////////////////////////////////////////////
+
+double NominaSimple::calcularHorasExtra(void)
+{
+   // TODO : implement
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       NominaSimple::calcularImpRenta()
+// Purpose:    Implementation of NominaSimple::calcularImpRenta()
+// Return:     double
+////////////////////////////////////////////////////////////////////////
+
+double NominaSimple::calcularImpRenta(void)
+{
+   // TODO : implement
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       NominaSimple::calcularIngresos()
+// Purpose:    Implementation of NominaSimple::calcularIngresos()
+// Return:     double
+////////////////////////////////////////////////////////////////////////
+
+double NominaSimple::calcularIngresos(void)
+{
+   // TODO : implement
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       NominaSimple::calcularEgresos()
+// Purpose:    Implementation of NominaSimple::calcularEgresos()
+// Return:     double
+////////////////////////////////////////////////////////////////////////
+
+double NominaSimple::calcularEgresos(void)
+{
+   // TODO : implement
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       NominaSimple::generarRolPagos()
+// Purpose:    Implementation of NominaSimple::generarRolPagos()
+// Return:     void
+////////////////////////////////////////////////////////////////////////
+
+void NominaSimple::generarRolPagos(void)
+{
+   // TODO : implement
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       NominaSimple::getPrimero()
+// Purpose:    Implementation of NominaSimple::getPrimero()
+// Return:     NodoSimple
+////////////////////////////////////////////////////////////////////////
+
+NodoSimple NominaSimple::getPrimero(void)
+{
+   return primero;
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       NominaSimple::setPrimero(NodoSimple newPrimero)
+// Purpose:    Implementation of NominaSimple::setPrimero()
+// Parameters:
+// - newPrimero
+// Return:     void
+////////////////////////////////////////////////////////////////////////
+
+void NominaSimple::setPrimero(NodoSimple newPrimero)
+{
+   primero = newPrimero;
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       NominaSimple::getActual()
+// Purpose:    Implementation of NominaSimple::getActual()
+// Return:     NodoSimple
+////////////////////////////////////////////////////////////////////////
+
+NodoSimple NominaSimple::getActual(void)
+{
+   return actual;
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       NominaSimple::setActual(NodoSimple newActual)
+// Purpose:    Implementation of NominaSimple::setActual()
+// Parameters:
+// - newActual
+// Return:     void
+////////////////////////////////////////////////////////////////////////
+
+void NominaSimple::setActual(NodoSimple newActual)
+{
+   actual = newActual;
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       NominaSimple::getListaVacia()
+// Purpose:    Implementation of NominaSimple::getListaVacia()
+// Return:     bool
+////////////////////////////////////////////////////////////////////////
+
+bool NominaSimple::getListaVacia(void)
+{
+   // TODO : implement
+}
---
+++ ProyectoP1/NominaSimple.h
@@ -0,0 +1,45 @@
+/***********************************************************************
+UFA - ESPE
+AUTORES: Matías Padrón,Sebastian Torres
+FECHA DE CREACIÓN: 19/11/2022
+FECHA DE MODIFICACIÓN: 19/11/2022
+Proyecto Unidad 1-Rol de pagos
+GITHUB: Padron-Torres-Primer-Parcial-Estructuras
+ ***********************************************************************/
+
+#if !defined(__Class_Diagram_1_NominaSimple_h)
+#define __Class_Diagram_1_NominaSimple_h
+
+#include <NodoSimple.h>
+
+class NominaSimple
+{
+public:
+   NominaSimple();
+   ~NominaSimple();
+   int insertarPorCabeza(void);
+   int eliminarPorCabeza(void);
+   int eliminarPorCola(void);
+   int mostrarNominaS(void);
+   double calcularIess(void);
+   double calcularHorasSup(void);
+   double calcularHorasExtra(void);
+   double calcularImpRenta(void);
+   double calcularIngresos(void);
+   double calcularEgresos(void);
+   void generarRolPagos(void);
+   NodoSimple getPrimero(void);
+   void setPrimero(NodoSimple newPrimero);
+   NodoSimple getActual(void);
+   void setActual(NodoSimple newActual);
+   bool getListaVacia(void);
+
+protected:
+private:
+   NodoSimple primero;
+   NodoSimple actual;
+
+
+};
+
+#endif
